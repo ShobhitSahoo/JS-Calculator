35fdd6f8f0727c9eafb693d72ce8ec2ce5beba9b
worachatsun <worachat.sun@gmail.com>
2020-10-20 05:36:50 +0900

    Change UI with bootstrap

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 window.addEventListener("DOMContentLoaded", init);
 
 // All the keys to be used
-const keys = ['*', '/', '+', '-', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '.'];
+const numbers = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0'];
+const operators = ['/', '*', '-', '+']
 
 // All the function keys
 const spec = ['*', '/', '+', '-'];
+
+function outputDoc() {
+    const output = document.createElement('input');
+    output.setAttribute('type', 'text');
+    output.setAttribute('class', 'form-control')
+    output.classList.add('output');
+    output.style.width = '100%';
+    output.style.lineHeight = '40px';
+    output.style.fontSize = '3em';
+    output.style.textAlign = 'right';
+    return output
+}
 
 function init() {
     console.log("Ready");
@@ -17,27 +30,51 @@ function init() {
     container.style.margin = 'auto';
     document.body.appendChild(container);
 
-    const output = document.createElement('input');
-    output.setAttribute('type', 'text');
-    output.classList.add('output');
-    output.style.width = '100%';
-    output.style.lineHeight = '50px';
-    output.style.fontSize = '3em';
-    output.style.textAlign = 'right';
+    const output = outputDoc()
     container.appendChild(output);
 
     const main = document.createElement('div');
-    main.classList.add('main');
-    main.style.width = '100%';
+    main.setAttribute('class', 'container')
     container.appendChild(main);
+    
+    const del = document.createElement('div');
+    del.setAttribute('class', 'row')
+    btnMaker('C', clrOutput, del);
+    btnMaker('Del', backSpace, del);
+    main.appendChild(del);
 
-    keys.forEach(function(val) {
-        btnMaker(val, addOutput);
+    let row
+    let colNums
+    numbers.forEach(function(val, index) {
+        if (index%3===0){
+            row = document.createElement('div');
+            row.setAttribute('class', 'row')
+            row.setAttribute('role', 'toolbar')
+            colNums = document.createElement('div');
+            colNums.setAttribute('class', 'btn-group mr-2 col-9')
+            colNums.setAttribute('role', 'group')
+            colNums.setAttribute('aria-label', 'group1')
+            row.appendChild(colNums);
+            row.style.margin = '5px 0';
+            let colOperator = document.createElement('div');
+            colOperator.setAttribute('class', 'btn-group  col')
+            colOperator.setAttribute('role', 'group')
+            colOperator.setAttribute('aria-label', 'group2')
+            let btn = document.createElement('button');
+            btn.setAttribute('type', 'button');
+            btn.setAttribute('class', 'btn btn-warning')
+            btn.val = operators[index/3];
+            btn.textContent = operators[index/3];
+            btn.addEventListener('click', addOutput);
+            colOperator.appendChild(btn);
+            row.appendChild(colOperator);
+            main.appendChild(row);
+        }
+        btnMaker(val, addOutput, colNums);
+        if(index===numbers.length-1){
+            btnMaker('=', evalOutput, colNums);
+        }
     })
-
-    btnMaker('=', evalOutput);
-    btnMaker('<<', backSpace);
-    btnMaker('C', clrOutput);
 
     function evalOutput() {
         output.style.border = '2px solid black';
@@ -70,17 +107,29 @@ function init() {
         }
     }
 
-    function btnMaker(txt, outputFunc) {
+    function btnMaker(txt, outputFunc, row) {
         let btn = document.createElement('button');
         btn.setAttribute('type', 'button');
         btn.style.width = '23%';
-        btn.style.lineHeight = '50px';
+        btn.style.lineHeight = '35px';
         btn.style.margin = '1%';
         btn.style.fontSize = '1.8em';
         btn.val = txt;
         btn.textContent = txt;
         btn.addEventListener('click', outputFunc);
-        main.appendChild(btn);
+        switch (txt) {
+            case 'C':
+            case "Del":
+                btn.setAttribute('class', 'btn btn-danger')
+                break;
+            case '=':
+                btn.setAttribute('class', 'btn btn-success')
+                break;
+            default:
+                btn.setAttribute('class', 'btn')
+                break;
+        }
+        row.appendChild(btn);
     }
 
     function addOutput(e) {
